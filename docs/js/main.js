2e30adc5020c797a42eec8505a732ad62aebf5bc
Kevin Mosqueda <kevmosqueda@hotmail.com>
2025-07-18 09:00:57 -0600

    fix UI visibility again

--- docs/js/main.js
+++ docs/js/main.js
@@ -106,6 +106,7 @@ document
     }
 
     document.getElementById("initialOverlay").classList.add("hidden");
+    document.querySelector(".content-sections").classList.remove("hidden");
     // Update volume slider to reflect HTML audio volume
     // Note: Tone.gainToDb(backgroundMusic.volume) converts 0-1 range to dB for slider
     document.getElementById("masterVolume").value = Tone.gainToDb(
